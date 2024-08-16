0e8c5f31ecdde6a269975b3aba2c1ec335b2b477
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-08-16 08:43:50 -0700

    cmd/diskcache: add a cache program using cachedir

---
+++ cmd/diskcache/diskcache.go
@@ -0,0 +1,47 @@
+// Program diskcache implements the Go toolchain cache protocol using a
+// local disk directory for storage.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/creachadair/gocache"
+	"github.com/creachadair/gocache/cachedir"
+	"github.com/creachadair/mds/value"
+)
+
+var (
+	cacheDir  = flag.String("cache-dir", "", "Cache directory (required)")
+	maxAge    = flag.Duration("x", 24*time.Hour, "Age after which cache entries expire")
+	doVerbose = flag.Bool("v", false, "Enable verbose logging")
+)
+
+func main() {
+	flag.Parse()
+
+	if *cacheDir == "" {
+		log.Fatal("You must provide a --cache-dir")
+	}
+
+	dir, err := cachedir.New(*cacheDir)
+	if err != nil {
+		log.Fatalf("Create cache dir: %v", err)
+	}
+	s := &gocache.Server{
+		Get:   dir.Get,
+		Put:   dir.Put,
+		Close: dir.Cleanup(*maxAge),
+
+		Logf: value.Cond(*doVerbose, log.Printf, nil),
+	}
+
+	if err := s.Run(context.Background(), os.Stdin, os.Stdout); err != nil {
+		log.Printf("Server exited with error: %v", err)
+	}
+	fmt.Fprintln(os.Stderr, s.Metrics())
+}
