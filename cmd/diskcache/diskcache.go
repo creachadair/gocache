d6819398e40fb60cf9c682d3f27409566cb60046
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-08-20 18:54:09 -0700

    cmd/diskcache: use the command package for nicer help

--- cmd/diskcache/diskcache.go
+++ cmd/diskcache/diskcache.go
@@ -3,50 +3,65 @@
 package main
 
 import (
-	"cmp"
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"runtime"
+	"time"
 
+	"github.com/creachadair/command"
+	"github.com/creachadair/flax"
 	"github.com/creachadair/gocache"
 	"github.com/creachadair/gocache/cachedir"
 	"github.com/creachadair/mds/value"
 )
 
-var (
-	cacheDir    = flag.String("cache-dir", "", "Cache directory (required)")
-	maxAge      = flag.Duration("x", 0, "Age after which cache entries expire")
-	doVerbose   = flag.Bool("v", false, "Enable verbose logging")
-	concurrency = flag.Int("c", 0, "Maximum number of concurrent requests")
-)
+var flags = struct {
+	CacheDir    string        `flag:"cache-dir,Cache directory (required)"`
+	Concurrency int           `flag:"c,default=*,Maximum number of concurrent requests"`
+	MaxAge      time.Duration `flag:"x,Age after which cache entries expire"`
+	Metrics     bool          `flag:"m,Print cache metrics to stderr on exit"`
+	Verbose     bool          `flag:"v,Enable verbose logging"`
+}{
+	Concurrency: runtime.NumCPU(),
+}
 
 func main() {
-	flag.Parse()
+	root := &command.C{
+		Name:     command.ProgramName(),
+		Usage:    "--cache-dir d [options]\nhelp",
+		Help:     `Serve a GOCACHEPROG plugin on stdin/stdout.`,
+		SetFlags: command.Flags(flax.MustBind, &flags),
+		Run: command.Adapt(func(env *command.Env) error {
+			if flags.CacheDir == "" {
+				return env.Usagef("You must provide a --cache-dir")
+			}
 
-	if *cacheDir == "" {
-		log.Fatal("You must provide a --cache-dir")
-	}
+			dir, err := cachedir.New(flags.CacheDir)
+			if err != nil {
+				return fmt.Errorf("create cache dir: %w", err)
+			}
+			s := &gocache.Server{
+				Get:         dir.Get,
+				Put:         dir.Put,
+				Close:       dir.Cleanup(flags.MaxAge),
+				MaxRequests: flags.Concurrency,
+				Logf:        value.Cond(flags.Verbose, log.Printf, nil),
+			}
 
-	dir, err := cachedir.New(*cacheDir)
-	if err != nil {
-		log.Fatalf("Create cache dir: %v", err)
+			if err := s.Run(context.Background(), os.Stdin, os.Stdout); err != nil {
+				log.Printf("Server exited with error: %v", err)
+			}
+			if flags.Verbose || flags.Metrics {
+				fmt.Fprintln(os.Stderr, s.Metrics())
+			}
+			return nil
+		}),
+		Commands: []*command.C{
+			command.HelpCommand(nil),
+			command.VersionCommand(),
+		},
 	}
-	s := &gocache.Server{
-		Get:   dir.Get,
-		Put:   dir.Put,
-		Close: dir.Cleanup(*maxAge),
-
-		MaxRequests: cmp.Or(*concurrency, runtime.NumCPU()),
-		Logf:        value.Cond(*doVerbose, log.Printf, nil),
-	}
-
-	if err := s.Run(context.Background(), os.Stdin, os.Stdout); err != nil {
-		log.Printf("Server exited with error: %v", err)
-	}
-	if *doVerbose {
-		fmt.Fprintln(os.Stderr, s.Metrics())
-	}
+	command.RunOrFail(root.NewEnv(nil), os.Args[1:])
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require github.com/creachadair/taskgroup v0.9.0
 
 require (
 	github.com/creachadair/atomicfile v0.3.4
+	github.com/creachadair/command v0.1.13
+	github.com/creachadair/flax v0.0.1
 	github.com/creachadair/mds v0.17.1
 	github.com/google/go-cmp v0.6.0
 )
